e9ba55da89156a13aacca8607dd372f59b036d99
Alka121 <Alkabajpayee2016@gmail.com>
2023-11-04 16:07:15 +0530

    first commit

---
+++ p3.py
@@ -0,0 +1,93 @@
+i="I"
+v="V"
+x="X"
+
+num=int(input("enter the number:"))
+
+if(num==1):
+  print(i)
+elif(num==2):
+  print(i+i)
+elif(num==3):
+  print(i+i+i)
+elif(num==4):
+  print(i+v)
+elif(num==5):
+  print(v) 
+elif(num==6):
+  print(v+i)  
+elif(num==7):
+  print(v+i+i)
+elif(num==8):
+  print(v+i+i+i)
+elif(num==9):
+  print(i+x) 
+elif(num==10):
+  print(x) 
+elif(num==11):
+  print(x+i)
+elif(num==12):
+  print(x+i+i)
+elif(num==13):
+  print(x+i+i+i)
+elif(num==14):
+  print(x+i+v) 
+elif(num==15):
+  print(x+v)  
+elif(num==16):
+  print(x+v+i)
+elif(num==17):
+  print(x+v+i+i)
+elif(num==18):
+  print(x+v+i+i)
+elif(num==19):
+  print(x+v+i+i+i)
+elif(num==20):
+  print(x+x)
+elif(num==21):
+  print(x+x+i)
+elif(num==22):
+  print(x+x+i+i)
+elif(num==23):
+  print(x+x+i+i+i)
+elif(num==24):
+  print(x+x+i+v) 
+elif(num==25):
+  print(x+x+v)  
+elif(num==26):
+  print(x+x+v+i)
+elif(num==27):
+  print(x+x+v+i+i)
+elif(num==28):
+  print(x+x+v+i+i)
+elif(num==29):
+  print(x+x+v+i+i+i)
+elif(num==30):
+  print(x+x+x)
+elif(num==31):
+  print(x+x+x+i)
+elif(num==32):
+  print(x+x+x+i+i)
+elif(num==33):
+  print(x+x+x+i+i+i)
+elif(num==34):
+  print(x+x+x+i+v) 
+elif(num==35):
+  print(x+x+x+v)  
+elif(num==36):
+  print(x+x+x+v+i)
+elif(num==37):
+  print(x+x+x+v+i+i)
+elif(num==38):
+  print(x+x+x+v+i+i)
+elif(num==39):
+  print(x+x+x+v+i+i+i)
+
+
+
+
+
+
+
+
+  
